e196238e3b38674aeec7c9742ae5a7ac39e5001a
Akira Taniguchi <dededededaiou2003@yahoo.co.jp>
2022-07-15 16:01:08 +0900

    Initial commit

---
+++ test/example-token.ts
@@ -0,0 +1,36 @@
+import { expect } from 'chai'
+import { ethers } from 'hardhat'
+import {
+	takeSnapshot,
+	SnapshotRestorer,
+} from '@nomicfoundation/hardhat-network-helpers'
+import { ExampleToken } from '../typechain-types'
+
+describe('Example', () => {
+	let example: ExampleToken
+	let snapshot: SnapshotRestorer
+	before(async () => {
+		const factory = await ethers.getContractFactory('ExampleToken')
+		example = (await factory.deploy()) as ExampleToken
+		await example.deployed()
+		await example.initialize()
+	})
+	beforeEach(async () => {
+		snapshot = await takeSnapshot()
+	})
+	afterEach(async () => {
+		await snapshot.restore()
+	})
+	describe('name', () => {
+		it('check name', async () => {
+			const value = await example.name()
+			expect(value.toString()).to.equal('token')
+		})
+	})
+	describe('symbol', () => {
+		it('check symbol', async () => {
+			const symbol = await example.symbol()
+			expect(symbol.toString()).to.equal('TOKEN')
+		})
+	})
+})
